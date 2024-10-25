ec09ddc5e30a4b613b6334e966d99b424c404486
Zynalex9 <alzn952@gmail.com>
2024-10-25 23:39:13 +0500

    Authjs customization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,9 @@ import Component from "./components/Compo";
 
 export default function Home() {
   return (
-    <div >
-     <h1>Hello World</h1>
-     <Component/>
-    </div>
+    <>
+      <h1>Hello World</h1>
+      <Component />
+    </>
   );
 }
