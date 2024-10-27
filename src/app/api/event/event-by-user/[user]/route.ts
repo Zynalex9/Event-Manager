04cd6ddfbd4fef9b5cb8a9732ea65635883f8b9a
Zynalex9 <alzn952@gmail.com>
2024-10-27 19:29:39 +0500

    Single user events

---
+++ src/app/api/event/event-by-user/[user]/route.ts
@@ -0,0 +1,36 @@
+import { NextRequest, NextResponse } from "next/server";
+import mongoose from "mongoose";
+import eventModel from "../../../../../../models/eventModel";
+
+export async function GET(
+  request: NextRequest,
+  { params }: { params: { user: string } }
+) {
+  try {
+    const { user } = params;
+
+    if (!mongoose.Types.ObjectId.isValid(user)) {
+      return NextResponse.json(
+        { success: false, message: `Invalid user ID '${user}'` },
+        { status: 400 }
+      );
+    }
+
+    const events = await eventModel.find({ host: user });
+
+    if (!events || events.length === 0) {
+      return NextResponse.json(
+        { success: false, message: `No events found for user '${user}'` },
+        { status: 404 }
+      );
+    }
+
+    return NextResponse.json({ success: true, events }, { status: 200 });
+  } catch (error:any) {
+    console.error("Error processing request:", error);
+    return NextResponse.json(
+      { message: "Error processing request", success: false, error: error?.message },
+      { status: 500 }
+    );
+  }
+}
