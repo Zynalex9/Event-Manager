feaf93a2fcaefc11e09de8af54a5d507cedc0581
Zynalex9 <alzn952@gmail.com>
2024-11-09 13:29:23 +0500

    Get Single user

---
+++ src/app/api/user/getUser/[user]/route.ts
@@ -0,0 +1,19 @@
+import { NextRequest, NextResponse } from "next/server";
+import userModel from "../../../../../../models/userModel";
+
+export async function GET( request: NextRequest, { params }: { params: { user: string } }) {
+  const { user } = params;
+  try {
+    const isUser = await userModel.findOne({ username:user });
+    if (!isUser) {
+      return NextResponse.json({
+        message: `No user found by username by ${user}`,
+        success:false
+      },{status:404});
+    }
+    return NextResponse.json({
+        message:"User found",
+        isUser
+      })
+  } catch (error) {}
+}
